5c8aed45059318f8c6e41e50cb923fbf59b9e005
jatin711-debug <69040077+jatin711-debug@users.noreply.github.com>
2022-03-16 20:06:38 -0400

    🙂Adding Google Cloud Build Config 🤪and setting up routes

--- auth/src/App.ts
+++ auth/src/App.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
 
 const app = express();
 
 app.use(json());
 
+app.use(currentUserRouter);
+
 app.listen(3000,()=>{
-    console.log("Listening on 3000!");
+    console.log("Listening on 3000!!!!!!");
 });
