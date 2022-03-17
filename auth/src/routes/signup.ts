27b9e799a585e5d3d1e940ee57becc7ded9d6e8b
jatin711-debug <69040077+jatin711-debug@users.noreply.github.com>
2022-03-17 00:10:06 -0400

    🧐 Regulating Request with express-validator

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,24 @@
-import express from 'express';
+import express, { Request,Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req,res) => {
+router.post('/api/users/signup',[
+    body('email')
+        .isEmail()
+            .withMessage('Email must be provided'),
+    body('password')
+        .trim()
+            .isLength({ min:4, max:20 })
+                .withMessage('Password must be at least between 4 and 20 characters')
+],(req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).send(errors.array());
+    }
+
+    const { email, password } = req.body;
+    console.log(password+ email);
     res.send("Hi There!");
 });
 
